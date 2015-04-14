e98aefe81555ec5e041de56b187a9718070cfff6
Michael Maroszek <mm@ip-symcon.de>
2015-04-14 10:43:22 +0200

    add variable action example

---
+++ VariableAction/module.php
@@ -0,0 +1,40 @@
+<?
+
+	class VariableAction extends IPSModule
+	{
+		
+		public function __construct($InstanceID)
+		{
+			//Never delete this line!
+			parent::__construct($InstanceID);
+			
+		}
+		
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+			//Lets register a variable with action
+			$this->RegisterVariableInteger("TestVariable", "Test", "~Intensity.100");
+			$this->EnableAction("TestVariable");
+			
+		}
+		
+		public function RequestAction($Ident, $Value)
+		{
+			
+			switch($Ident) {
+				case "TestVariable":
+					SetValue($this->GetIDForIdent($Ident), $Value);
+					break;
+				default:
+					throw new Exception("Invalid ident");
+			}
+		
+		}
+		
+	
+	}
+
+?>
