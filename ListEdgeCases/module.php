147ac4c75ed1759c06ddd3d7a9dfa0bd341fb50e
Michael Maroszek <maroszek@gmx.net>
2017-04-07 16:21:53 +0200

    add test cases for Select, SelectColor and SelectFile in Lists

---
+++ ListEdgeCases/module.php
@@ -0,0 +1,15 @@
+<?
+
+	class ListEdgeCases extends IPSModule
+	{
+		
+		public function Create() {
+			//Never delete this line!
+			parent::Create();
+			
+			$this->RegisterPropertyString("TreeData", "");
+
+		}
+	}
+	
+?>
