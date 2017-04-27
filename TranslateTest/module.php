2380d47d97351f8e095f68a88fc1240841510d90
Michael Maroszek <maroszek@gmx.net>
2017-04-27 09:44:40 +0200

    add new Translation Test module

---
+++ TranslateTest/module.php
@@ -0,0 +1,17 @@
+<?
+
+	class TranslateTest extends IPSModule
+	{
+		
+		public function GetConfigurationForm()
+		{
+			
+			$data = json_decode(file_get_contents(__DIR__ . "/form.json"));
+			$data->actions[0]->label = sprintf($this->Translate("The current time is %s"), date("d.m.y H:i"));
+			return json_encode($data);
+		}
+		
+	
+	}
+
+?>
