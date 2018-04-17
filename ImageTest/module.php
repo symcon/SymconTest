d316f4704be80c3dfc94327b0d1e32454f2ddf46
Niels Thole <niels.thole@gmail.com>
2018-04-17 11:24:19 +0200

    Tests for new form fields
    LayoutTest: PopupButton, ExpansionPanel, and RowLayout
    ImageTest: Image

---
+++ ImageTest/module.php
@@ -0,0 +1,20 @@
+<?
+class ImageTest extends IPSModule {
+    
+    public function Create(){
+        //Never delete this line!
+        parent::Create();
+    }
+
+    public function Destroy(){
+        //Never delete this line!
+        parent::Destroy();
+    }
+
+    public function ApplyChanges(){
+        //Never delete this line!
+        parent::ApplyChanges();
+    }
+}
+
+?>
---
+++ LayoutTest/module.php
@@ -0,0 +1,27 @@
+<?
+class LayoutTest extends IPSModule {
+    
+    public function Create(){
+        //Never delete this line!
+        parent::Create();
+        
+        //These lines are parsed on Symcon Startup or Instance creation
+        //You cannot use variables here. Just static values.
+        $this->RegisterPropertyInteger("Selection", 0);
+        $this->RegisterPropertyInteger("VariableTest", 0);
+        $this->RegisterPropertyString("File", "");
+        
+    }
+
+    public function Destroy(){
+        //Never delete this line!
+        parent::Destroy();
+    }
+
+    public function ApplyChanges(){
+        //Never delete this line!
+        parent::ApplyChanges();
+    }
+}
+
+?>
