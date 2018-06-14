07c5c9d3029b9e1433e5ca829658dd03b82967d9
Niels Thole <niels.thole@gmail.com>
2018-06-14 12:43:14 +0200

    Add tests for Configurator

---
+++ ConfiguratorChainTest/module.php
@@ -0,0 +1,13 @@
+<?
+
+    class ConfiguratorChainTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+        }
+    }
+
+?>
---
+++ ConfiguratorSimpleTest/module.php
@@ -0,0 +1,13 @@
+<?
+
+    class ConfiguratorSimpleTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+        }
+    }
+
+?>
