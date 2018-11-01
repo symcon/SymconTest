fa125c09dce9514a7eece425a4e42dea86309800
Niels Thole <niels.thole@gmail.com>
2018-11-01 10:28:17 +0100

    Fix formatting and button output of attribute test

--- AttributeTest/module.php
+++ AttributeTest/module.php
@@ -1,32 +1,32 @@
 <?
 
-	class AttributeTest extends IPSModule
-	{
-		
-		public function Create() {
-			//Never delete this line!
-			parent::Create();
-			
+    class AttributeTest extends IPSModule
+    {
+        
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+            
             $this->RegisterAttributeBoolean("BoolAttr", true);
             $this->RegisterAttributeInteger("IntAttr", 5);
-			$this->RegisterAttributeFloat("FloatAttr", 3.7);
+            $this->RegisterAttributeFloat("FloatAttr", 3.7);
             $this->RegisterAttributeString("StrAttr", "lalala");
-			
-		}
-		
-		public function BumpAndShow() {
-			
-			var_dump($this->ReadAttributeBoolean("BoolAttr"));
+            
+        }
+        
+        public function BumpAndShow() {
+            
+            var_dump($this->ReadAttributeBoolean("BoolAttr"));
             var_dump($this->ReadAttributeInteger("IntAttr"));
             var_dump($this->ReadAttributeFloat("FloatAttr"));
             var_dump($this->ReadAttributeString("StrAttr"));
             
             $this->WriteAttributeBoolean("BoolAttr", !$this->ReadAttributeBoolean("BoolAttr"));
-			$this->WriteAttributeInteger("IntAttr", $this->ReadAttributeInteger("IntAttr")*2);
+            $this->WriteAttributeInteger("IntAttr", $this->ReadAttributeInteger("IntAttr")*2);
             $this->WriteAttributeFloat("FloatAttr", $this->ReadAttributeFloat("FloatAttr")+0.1);
             $this->WriteAttributeString("StrAttr", $this->ReadAttributeString("StrAttr") . "öäü");
             
-		}
-	}
-	
+        }
+    }
+    
 ?>
