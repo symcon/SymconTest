4be2b6bcedaed44532569dbc8f44727dbdc2380b
Michael Maroszek <maroszek@gmx.net>
2018-11-07 09:04:54 +0100

    add reference test

---
+++ ReferenceTest/module.php
@@ -0,0 +1,20 @@
+<?
+
+    class ReferenceTest extends IPSModule
+    {
+        
+        public function ApplyChanges() {
+
+            $refs = $this->GetReferenceList();
+            foreach($refs as $ref) {
+                $this->UnregisterReference($ref);
+            }
+            $ids = IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}");
+            foreach($ids as $id) {
+                $this->RegisterReference($id);
+            }
+            
+        }
+    }
+    
+?>
