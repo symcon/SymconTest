96c858653a5fb2e0059cf3b912e4a50f1e969962
Michael Maroszek <maroszek@gmx.net>
2019-02-25 20:50:08 -0500

    add example for timers without using a public function

---
+++ TimerTest/module.php
@@ -0,0 +1,33 @@
+<?
+
+	class TimerTest extends IPSModule
+	{
+
+		public function Create() {
+
+			// Diese Zeile nicht entfernen
+			parent::Create();
+			
+			// Dieses Beispiel zeigt einen Trick, wie man die protected Funktion "TimerCaller" aus einem Timer starten kann.
+			// Einfacher ist die Funktion public zu deklarieren und dann per Prefix_FunktionsName($id) aufzurufen.
+			$magic = __FILE__;
+			$this->RegisterTimer("Test", 10 * 1000, <<<EOT
+				include("$magic");
+				class TranslateTestOverride extends TimerTest {
+					public function TimerCaller() { 
+						parent::TimerCaller();
+					}
+				}
+				(new TranslateTestOverride(\$_IPS['TARGET']))->TimerCaller();
+EOT
+);
+
+		}
+		
+		protected function TimerCaller() {
+			IPS_LogMessage("TimerTest", "Testing!");
+		}
+	
+	}
+
+?>
