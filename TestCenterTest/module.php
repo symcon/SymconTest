155ee4fac45943099df5425d59465f12469a208a
Niels Thole <niels.thole@gmail.com>
2019-03-07 16:26:18 +0100

    Add a test for the TestCenter

---
+++ TestCenterTest/module.php
@@ -0,0 +1,41 @@
+<?
+
+    class TestCenterTest extends IPSModule
+    {
+
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+            $this->RegisterVariableBoolean("SomeBool", "Some Boolean");
+            $this->EnableAction("SomeBool");
+            $this->RegisterVariableBoolean("SomeBoolProfile", "Some Boolean", '~Switch');
+            $this->EnableAction("SomeBoolProfile");
+            $this->RegisterVariableInteger("SomeEnumeration", "Some Enumeration", "~ShutterPosition.100");
+            $this->EnableAction("SomeEnumeration");
+            $this->RegisterVariableInteger("SomeDate", "Some Date", "~UnixTimestampDate");
+            $this->EnableAction("SomeDate");
+            $this->RegisterVariableInteger("SomeDateTime", "Some Date/Time", "~UnixTimestamp");
+            $this->EnableAction("SomeDateTime");
+            $this->RegisterVariableInteger("SomeTime", "Some Time", "~UnixTimestampTime");
+            $this->EnableAction("SomeTime");
+            $this->RegisterVariableInteger("SomeColor", "Some Color", "~HexColor");
+            $this->EnableAction("SomeColor");
+            $this->RegisterVariableInteger("SomePercentage", "Some Percentage", "~Intensity.100");
+            $this->EnableAction("SomePercentage");
+            $this->RegisterVariableFloat("SomeFloat", "Some Float");
+            $this->EnableAction("SomeFloat");
+            $this->RegisterVariableInteger("SomeInt", "Some Integer");
+            $this->EnableAction("SomeInt");
+            $this->RegisterVariableString("SomeString", "Some String");
+            $this->EnableAction("SomeString");
+        }
+
+
+        public function RequestAction($Ident, $Value)
+        {
+            SetValue($this->GetIDForIdent($Ident), $Value);
+        }
+    }
+    
+?>
