ea94eef28b52713be5b7c5fb0d81f1576af44a3a
Niels Thole <niels.thole@gmail.com>
2019-07-09 16:35:27 +0200

    Translate Test for lists and trees

---
+++ TranslateListTest/module.php
@@ -0,0 +1,17 @@
+<?
+
+	class TranslateListTest extends IPSModule
+	{
+		
+		public function Create() {
+			//Never delete this line!
+			parent::Create();
+			
+			$this->RegisterPropertyString("TranslateList", "[]");
+			$this->RegisterPropertyString("TranslateTree", "[]");
+
+		}
+	
+	}
+
+?>
