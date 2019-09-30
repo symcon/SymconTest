fc67906ffbb0e018e5a1c1494f3f015fc1cf97fd
Niels Thole <niels.thole@gmail.com>
2019-09-30 14:03:14 +0200

    ValidateTest

---
+++ ValidateTest/module.php
@@ -0,0 +1,13 @@
+<?
+
+    class ValidateTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+        }
+    }
+
+?>
