58c5c6a40dd5cd11bc735af6e369312ef7c72696
Niels Thole <niels.thole@gmail.com>
2019-10-16 10:32:28 +0200

    Add new test for links in labels and new parameter "link" for Label

---
+++ DynamicLinkLabelTest/module.php
@@ -0,0 +1,14 @@
+<?
+
+    class DynamicLinkLabelTest extends IPSModule
+    {
+        public function UpdateLabel($Text) {
+            $this->UpdateFormField('Label', 'caption', $Text);
+        }
+
+        public function UpdateLink($Link) {
+            $this->UpdateFormField('Label', 'link', $Link);
+        }
+    }
+    
+?>
