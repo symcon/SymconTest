ac40bb38ae3157890711fedea0b5a3d9ecfb8ab1
Niels Thole <niels.thole@gmail.com>
2019-11-05 13:48:54 +0100

    Expanded Umlauts-Test

--- ListUmlauts/module.php
+++ ListUmlauts/module.php
@@ -11,7 +11,17 @@
 		}
 
 		public function UpdateValue() {
-			$this->UpdateFormField('Umlauts', 'values', json_encode([['text' => 'Würstchenßßäöü']]));
+		   $newValue = json_encode([['text' => 'Würstchenßßäöü']]);
+		   $oldValue = json_encode(json_decode($this->ReadPropertyString('Umlauts')));
+		   $this->SendDebug('Umlauts' . '(prop)', $this->ReadPropertyString('Umlauts'), 0);
+		   $this->SendDebug('Umlauts' . '(new)', $newValue, 0);
+		
+		   if ($newValue !== $oldValue){
+			  echo "Neu und alt sind ungleich". PHP_EOL;
+			  $this->UpdateFormField('Umlauts', 'values', $newValue);
+		   } else {
+			  echo "Neu und alt sind gleich". PHP_EOL;
+		   }
 		}
 	}
 
