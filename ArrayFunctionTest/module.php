ebec294d8476419074e88af50c05e5e6145dfdc1
Niels Thole <nt@symcon.de>
2020-07-17 14:03:49 +0200

    Implement Test Module that uses functions with array input / output

---
+++ ArrayFunctionTest/module.php
@@ -0,0 +1,15 @@
+<?
+
+    class ArrayFunctionTest extends IPSModule
+    {
+        
+        public function ArrayResult() : array {
+            return [1, 2, 3];
+        }
+
+        public function ArrayInput(array $data) {
+            
+        }
+    }
+    
+?>
