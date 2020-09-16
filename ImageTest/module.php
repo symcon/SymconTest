1a530ad043de6678d18446bace1425e1bf0b1134
Niels Thole <nt@symcon.de>
2020-09-16 15:15:39 +0200

    Expand ImageTest by an image from a media object

--- ImageTest/module.php
+++ ImageTest/module.php
@@ -4,6 +4,28 @@ class ImageTest extends IPSModule {
     public function Create(){
         //Never delete this line!
         parent::Create();
+
+        $this->RegisterPropertyInteger('media', 0);
+    }
+
+    public function GetConfigurationForm() {
+        return json_encode([
+            "elements" => [
+                [
+                    "type" => "Image",
+                    "image" => "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+                ],
+                [
+                    "type" => "SelectMedia",
+                    "name" => "media",
+                    "caption" => "Image/Stream below"
+                ],
+                [
+                    "type" => "Image",
+                    "mediaID" => $this->ReadPropertyInteger('media')
+                ]
+            ]
+        ]);
     }
 
     public function Destroy(){
