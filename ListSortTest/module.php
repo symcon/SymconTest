f28dd07e7ce8e81114a4e408a9cb4c8ab8529e58
Till Brede <tb@symcon.de>
2020-09-16 14:26:50 +0200

    added list test for StringLex

---
+++ ListSortTest/module.php
@@ -0,0 +1,15 @@
+<?
+
+	class ListSortTest extends IPSModule
+	{
+		
+		public function Create() {
+			//Never delete this line!
+			parent::Create();
+			
+			$this->RegisterPropertyString("SortList", "");
+
+		}			
+	}
+
+?>
