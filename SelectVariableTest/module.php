90e8680ca7141062b186cc6e751de5525b767589
TillBrede <51406030+TillBrede@users.noreply.github.com>
2020-12-14 15:16:01 +0100

    fixed style

--- SelectVariableTest/module.php
+++ SelectVariableTest/module.php
@@ -1,34 +1,34 @@
-<?
+<?php
 
-	class SelectVariableTest extends IPSModule
-	{
-		
-		public function Create() {
-			//Never delete this line!
-			parent::Create();
+    class SelectVariableTest extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
 
             $this->RegisterPropertyInteger('SelectIntegerFloat', 0);
-			
-		}
+        }
 
-		public function ChangeAction($action) {
-			$this->UpdateFormField('dynamic', 'requiredAction', $action);
-		}
+        public function ChangeAction($action)
+        {
+            $this->UpdateFormField('dynamic', 'requiredAction', $action);
+        }
 
-		
-		public function ChangeLogging($logging) {
-			$this->UpdateFormField('dynamic', 'requiredLogging', $logging);
-		}
+        
+        public function ChangeLogging($logging)
+        {
+            $this->UpdateFormField('dynamic', 'requiredLogging', $logging);
+        }
 
-		public function UpdateTypes($types) {
-			$newTypes = [];
-			for($i = 0; $i <count($types); $i++) {
-				if ($types[$i]) {
-					$newTypes[]= $i;
-				}
-			}
-			$this->UpdateFormField('dynamic', 'validVariableTypes', json_encode($newTypes));
-		}
-	}
-	
-?>
+        public function UpdateTypes($types)
+        {
+            $newTypes = [];
+            for ($i = 0; $i <count($types); $i++) {
+                if ($types[$i]) {
+                    $newTypes[]= $i;
+                }
+            }
+            $this->UpdateFormField('dynamic', 'validVariableTypes', json_encode($newTypes));
+        }
+    }
