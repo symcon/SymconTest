5b46bc8768953457fa201085695fc3dc783e7da1
DrNiels <nt@symcon.de>
2021-04-30 15:45:51 +0200

    Test for SelectCondition

---
+++ SelectConditionTest/module.php
@@ -0,0 +1,13 @@
+<?php
+
+    class SelectConditionTest extends IPSModule
+    {
+        
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyString("Condition", "");
+            
+        }
+    }
