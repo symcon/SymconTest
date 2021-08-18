1c4f15770b57f52f69213b82c559d5e518e5a328
Michael Maroszek <maroszek@gmx.net>
2021-08-18 17:36:00 +0200

    add DataWaitTest (Requires IP-Symcon 6.1)

---
+++ DataWaitTest/module.php
@@ -0,0 +1,23 @@
+<?
+
+	class DataWaitTest extends IPSModule
+	{
+		public function Create() {
+		 
+			// Diese Zeile nicht entfernen
+			parent::Create();
+		 
+			// Erstellt einen Timer mit dem Namen "Update" und einem Intervall von 5 Sekunden. 
+			$this->RegisterTimer("SendData", 5000, "DWT_Send(\$_IPS['TARGET']);");
+		}
+
+		public function Send() {
+			$this->NotifyWithData("SuperName", "Cool, es geht!");
+		}
+		
+		public function Wait() {
+			return $this->WaitForData("SuperName", 10000);
+		}
+	}
+	
+?>
