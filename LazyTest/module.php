32f4b2a807c730217e845c78871a91d3d5c84f26
Niels Thole <nt@symcon.de>
2021-10-18 10:35:43 +0200

    Add a test for lazy loaded elements

---
+++ LazyTest/module.php
@@ -0,0 +1,14 @@
+<?
+
+    class LazyTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyString('Text', '');
+        }
+    }
+
+?>
