cd7b65b13475b07f24fb4269433fce24f4e62c01
Niels Thole <nt@symcon.de>
2022-09-05 16:51:11 +0200

    Test OpenObjectButton from within a popup

---
+++ OpenObjectFromPopupTest/module.php
@@ -0,0 +1,46 @@
+<?
+
+    class OpenObjectFromPopupTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyBoolean('Visible', false);
+        }
+
+
+
+        public function GetConfigurationForm() {
+            return json_encode([
+                'actions' => [
+                    [
+                        'type' => 'PopupButton',
+                        'caption' => 'Open Popup',
+                        'popup' => [
+                            'items' => [
+                                [
+                                    'type' => 'OpenObjectButton',
+                                    'caption' => 'Open Connect',
+                                    'objectID' => IPS_GetInstanceListByModuleID('{9486D575-BE8C-4ED8-B5B5-20930E26DE6F}')[0]
+                                ],
+                                [
+                                    'type' => 'Configurator',
+                                    'values' => [
+                                        [
+                                            'name' => 'Connect',
+                                            'address' => '',
+                                            'instanceID' => IPS_GetInstanceListByModuleID('{9486D575-BE8C-4ED8-B5B5-20930E26DE6F}')[0]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+        }
+    }
+
+?>
