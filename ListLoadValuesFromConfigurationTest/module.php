dd178d50083a93dc4e99b9c4497a2355fcab80a6
Michael Tröger <mt@neo-ami.de>
2023-02-20 22:52:20 +0100

    no message

--- ListLoadValuesFromConfigurationTest/module.php
+++ ListLoadValuesFromConfigurationTest/module.php
@@ -14,5 +14,23 @@ class ListLoadValuesFromConfigurationTest extends IPSModule
     public function ApplyChanges() {
         //Never delete this line!
         parent::ApplyChanges();
-    }        
+    }   
+
+    public function GetConfigurationForm()
+    {
+        $Form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+        // Fehler tritt nur auf wenn
+        // Liste in ExpansionPanel
+        // UND onClick in ExpansionPanel
+        // UND ExpansionPanel beim laden geschlossen ist
+        // UND
+        // {
+        // 'values' in form.json vorhanden als "values": [] 
+        // ODER
+        // GetConfigurationForm setzt 'values' auf []
+        // }
+        //$Values=[];
+        //$Form['elements'][0]['items'][0]['values'] = $Values;
+        return json_encode($Form);
+    }
 }
