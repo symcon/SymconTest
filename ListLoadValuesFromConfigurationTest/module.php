05ae9340d2f660eb634b7bfb64319ebc37b2741e
Niels Thole <nt@symcon.de>
2023-02-20 11:06:29 +0100

    Add test for loadValuesFromConfiguration

---
+++ ListLoadValuesFromConfigurationTest/module.php
@@ -0,0 +1,18 @@
+<?php
+
+class ListLoadValuesFromConfigurationTest extends IPSModule
+{
+    
+    public function Create() {
+        //Never delete this line!
+        parent::Create();
+
+        $this->RegisterPropertyString('Targets', '[]');
+
+    }
+
+    public function ApplyChanges() {
+        //Never delete this line!
+        parent::ApplyChanges();
+    }        
+}
