b807224c01efce4efeec6eb7d1a519f311dc6496
Niels Thole <nt@symcon.de>
2023-05-24 11:59:59 +0200

    Test to validate that elements without corresponding property do not trigger the apply changes toast

---
+++ PropertyUpdateTest/module.php
@@ -0,0 +1,19 @@
+<?
+
+    class PropertyUpdateTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyString('Property1', 'Value');
+            $this->RegisterPropertyString('Property2', 'Value');
+        }
+        
+        public function UpdateValue($Field, $Value) {
+            $this->UpdateFormField($Field, 'value', $Value);
+        }
+    }
+
+?>
