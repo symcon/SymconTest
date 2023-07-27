b41cd9d60aaf0be5d9bcb1edafc5e558db4fd435
DrNiels <nt@symcon.de>
2023-07-27 15:13:32 +0200

    Finalize edit form test

--- ListDynamicEditFormTest/module.php
+++ ListDynamicEditFormTest/module.php
@@ -2,13 +2,22 @@
 
     class ListDynamicEditFormTest extends IPSModule
     {
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+            
+            $this->RegisterPropertyString("infos", "");
+
+        }
+
         public function UpdateInfos($infos) {
             $newInfos = [];
             foreach ($infos as $info) {
                 $newInfos[] = [
                     'info1' => $info['info1'],
                     'info2' => $info['info2'],
-                    'info' => $info['info1'] . $info['info2']
+                    'info' => $info['info1'] . $info['info2'],
+                    'active' => $info['active']
                 ];
             }
             $this->UpdateFormField('infos', 'values', json_encode($newInfos));
