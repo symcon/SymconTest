92aed7eca77efbff5c211fa5fe492fc002418d38
DrNiels <nt@symcon.de>
2023-07-27 14:14:56 +0200

    Test for onExpand

---
+++ OnExpandTest/module.php
@@ -0,0 +1,13 @@
+<?
+
+    class OnExpandTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+        }
+    }
+
+?>
