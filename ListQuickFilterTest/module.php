c5b068a9c845e80ddc905af0071ed38bf4b0f7ef
Niels Thole <nt@symcon.de>
2024-07-05 12:42:05 +0200

    Test for quick filter in List and Configurator

---
+++ ListQuickFilterTest/module.php
@@ -0,0 +1,12 @@
+<?php
+
+    class ListQuickFilterTest extends IPSModule
+    {
+        
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyString('List', '[]');
+        }
+    }
