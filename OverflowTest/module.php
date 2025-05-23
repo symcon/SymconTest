455468238ca54b98d464ae735bd84aa17f64e5c9
Niels Thole <nt@symcon.de>
2025-05-23 13:02:00 +0200

    Add test to validate that error overflow is properly displayed

---
+++ OverflowTest/module.php
@@ -0,0 +1,12 @@
+<?php
+
+    class OverflowTest extends IPSModule
+    {
+
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+        }
+    }
+
+?>
